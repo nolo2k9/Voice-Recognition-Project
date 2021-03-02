3d1a17a2e9f8acad2eb7c1c1354fd432d9e1fa32
Keith Nolan <keithnolan91@gmail.com>
2021-03-02 17:50:24 +0000

    Initial Setup
    Initial set up completed, sprites created and imported. First grammar completed

---
+++ VoiceControlProject/Assets/_Scripts/GrammerScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Text;
+
+public class GrammerScript : MonoBehaviour
+{
+    private GrammarRecognizer gr;
+
+    private void Start(){
+        Debug.Log("puss");
+        gr = new GrammarRecognizer(Application.streamingAssetsPath + "/SimpleGrammar.xml", ConfidenceLevel.Medium);
+        gr.OnPhraseRecognized += GR_OnPhraseRecognized;
+        gr.Start();
+        Debug.Log("Grammer loaded and recogniser started");
+
+    }
+
+    private void GR_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        StringBuilder message = new StringBuilder();
+        SemanticMeaning[] meanings = args.semanticMeanings;
+
+        foreach(SemanticMeaning meaning in meanings)
+        {
+            string keyString = meaning.key.Trim();
+            string valueString = meaning.values[0].Trim();
+            message.Append("Key: " + keyString + ", Value: " + valueString );
+
+        }
+
+        Debug.Log(message);
+    }
+
+    private void OnApplicationQuit()
+    {
+        if(gr != null && gr.IsRunning)
+        {
+            gr.OnPhraseRecognized -= GR_OnPhraseRecognized;
+            gr.Stop();
+        }
+
+       
+    }
+
+
+
+}
